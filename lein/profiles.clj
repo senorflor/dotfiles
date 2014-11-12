6a02fc68d5fe1dbf9250d16c1829330f7ee86c73
Patrick Flor <git.push.origin@gmail.com>
2014-11-12 17:18:32 -0600

    Add lein exec

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -2,7 +2,8 @@
                   [jonase/eastwood "0.1.4"]
                   [lein-try "0.4.3"]
                   [cider/cider-nrepl "0.8.0-SNAPSHOT"]
-                  [lein-gorilla "0.3.3"]]
+                  [lein-gorilla "0.3.3"]
+                  [lein-exec "0.3.4"]]
         :dependencies [[slamhound "1.5.5"]
                        [criterium "0.4.3"]]
         :repl-options {:init (require '[criterium.core :as crit])}}}
