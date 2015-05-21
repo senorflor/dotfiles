2027d494c068e750181f335f3e4900c35d5fb211
Patrick Flor <git.push.origin@gmail.com>
2015-05-21 13:13:15 -0500

    Bump and fix cider and deps

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,9 +1,10 @@
 {:user {:plugins [;;[lein-nodisassemble "0.1.2"]
                   [jonase/eastwood "0.1.4"]
                   [lein-try "0.4.3"]
-                  [cider/cider-nrepl "0.8.0-SNAPSHOT"]
+                  [cider/cider-nrepl "0.9.0-SNAPSHOT"]
                   [lein-gorilla "0.3.3"]
                   [lein-exec "0.3.4"]]
         :dependencies [[slamhound "1.5.5"]
-                       [criterium "0.4.3"]]
+                       [criterium "0.4.3"]
+                       [org.clojure/tools.nrepl "0.2.10"]]
         :repl-options {:init (require '[criterium.core :as crit])}}}
